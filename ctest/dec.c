215cef82a50836f47f88f7041a300484eb00f317
teyter <teg6@hi.is>
2025-09-09 14:55:19 +0000

    dec

--- ctest/dec.c
+++ ctest/dec.c
@@ -11,27 +11,33 @@ int power(int base, int exp)
     return base;
 }
 
-int convert(char *str)
+int main() 
 {
+    char *s;
+    s = "010";
+    s = "100";
+    s = "0xFF";
+    s = "0b1001";
     int m;
-    int strl = strlen(str);
+    int strl = strlen(s);
     int *numbers[strl];
-    if (str[1] == 'b')
+    if (s[1] == 'b')
     {
         printf("%s\n", "BINARY");
         m = 2;
+        /**
         for (int i = 2; i < strl; i++)
         {
-            numbers[i] = (int)str[i] - 48; // error
-            printf("%d ",str[i] - 48);
+            switch ()
         }
+         */
     }
-    else if (str[1] == 'x')
+    else if (s[1] == 'x')
     {
         printf("%s\n", "HEX");
         m = 16;
     }
-    else if (str[0] == '0')
+    else if (s[0] == '0')
     {
         printf("%s\n", "OCTAL");
         m = 8;
@@ -45,22 +51,10 @@ int convert(char *str)
     int sum = 0;
     for (int i = 0; i < strl; i++)
     {
-        sum += (str[i]-48) * power(m, e);
-        printf("sum += %d * %d^%d\n", (int)str[i], m, e);
+        sum += (s[i]-48) * power(m, e);
+        printf("sum += %d * %d^%d\n", (int)s[i], m, e);
         e--;
     }
     printf("%d\n",sum); 
-    return sum;
-}
-
-int main() 
-{
-    char *s;
-    s = "010";
-    s = "100";
-    s = "0xFF";
-    s = "0b1001";
-    convert(s);
-    // int f = power(7, 2);
     return 0;
 }
