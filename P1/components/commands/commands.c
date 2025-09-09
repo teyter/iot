396a3a5ccd66d664efdf8352393a14355b1d8c5f
teyter <teg6@hi.is>
2025-09-09 16:03:45 +0000

    p1 dec

--- P1/components/commands/commands.c
+++ P1/components/commands/commands.c
@@ -174,7 +174,99 @@ int process_cmd_status(const Command* in_cmd, char* out_msg) {
 	return 0;
 }
 
+int power(int base, int exp)
+{
+    if (exp == 0) return 1;
+    int x = base;
+    for (int i = 0; i < exp-1; i++)
+    {
+        base *= x;
+    }
+    return base;
+}
+
+int bconvert(char *s)
+{
+    int base;
+    int e;
+    int len = strlen(s);
+    int j = 2;
+    if (s[1] == 'b') {base = 2; e = len-3;} else if (s[1] == 'x') {base = 16;e=len-3;} else {base = 8;e=len-2;j=1;}
+    int sum = 0;
+    for (int i = j; i < len; i++)
+    {
+        if (s[i] == '0')
+        {
+            sum += 0 * power(base, e);
+        } 
+        else if (s[i] == '1')
+        {
+            sum += 1 * power(base, e);
+        }
+        else if (s[i] == '2')
+        {
+            sum += 2 * power(base, e);
+        }
+        else if (s[i] == '3')
+        {
+            sum += 3 * power(base, e);
+        }
+        else if (s[i] == '4')
+        {
+            sum += 4 * power(base, e);
+        }
+        else if (s[i] == '5')
+        {
+            sum += 5 * power(base, e);
+        }
+        else if (s[i] == '6')
+        {
+            sum += 6 * power(base, e);
+        }
+        else if (s[i] == '7')
+        {
+            sum += 7 * power(base, e);
+        }
+        else if (s[i] == '8')
+        {
+            sum += 8 * power(base, e);
+        }
+        else if (s[i] == '9')
+        {
+            sum += 9 * power(base, e);
+        }
+        else if (s[i] == 'A' || s[i] == 'a')
+        {
+            sum += 10 * power(base, e);
+        }
+        else if (s[i] == 'B' || s[i] == 'b')
+        {
+            sum += 11 * power(base, e);
+        }
+        else if (s[i] == 'C' || s[i] == 'c')
+        {
+            sum += 12 * power(base, e);
+        }
+        else if (s[i] == 'D' || s[i] == 'd')
+        {
+            sum += 13 * power(base, e);
+        }
+        else if (s[i] == 'E' || s[i] == 'e')
+        {
+            sum += 14 * power(base, e);
+        }
+        else if (s[i] == 'F' || s[i] == 'f')
+        {
+            sum += 15 * power(base, e);
+        }
+        e--;
+    }
+    return sum;
+}
+
 int process_cmd_dec(const Command* in_cmd, char* out_msg) {
     if (in_cmd->argument[0] != '0') return 2;
+	int out = bconvert(in_cmd->argument);
+	printf("%d\n", out);
 	return 0;
 }
--- ctest/dec.c
+++ ctest/dec.c
@@ -20,94 +20,75 @@ int bconvert(char *s)
     int j = 2;
     if (s[1] == 'b') {base = 2; e = len-3;} else if (s[1] == 'x') {base = 16;e=len-3;} else {base = 8;e=len-2;j=1;}
     int sum = 0;
-    printf("=============%d\n", e);
     for (int i = j; i < len; i++)
     {
         if (s[i] == '0')
         {
             sum += 0 * power(base, e);
-            printf("sum += 0 * %d^%d\n", base, e);
         } 
         else if (s[i] == '1')
         {
             sum += 1 * power(base, e);
-            printf("sum += 1 * %d^%d\n", base, e);
         }
         else if (s[i] == '2')
         {
             sum += 2 * power(base, e);
-            printf("sum += 2 * %d^%d\n", base, e);
         }
         else if (s[i] == '3')
         {
             sum += 3 * power(base, e);
-            printf("sum += 3 * %d^%d\n", base, e);
         }
         else if (s[i] == '4')
         {
             sum += 4 * power(base, e);
-            printf("sum += 4 * %d^%d\n", base, e);
         }
         else if (s[i] == '5')
         {
             sum += 5 * power(base, e);
-            printf("sum += 5 * %d^%d\n", base, e);
         }
         else if (s[i] == '6')
         {
             sum += 6 * power(base, e);
-            printf("sum += 6 * %d^%d\n", base, e);
         }
         else if (s[i] == '7')
         {
             sum += 7 * power(base, e);
-            printf("sum += 7 * %d^%d\n", base, e);
         }
         else if (s[i] == '8')
         {
             sum += 8 * power(base, e);
-            printf("sum += 8 * %d^%d\n", base, e);
         }
         else if (s[i] == '9')
         {
             sum += 9 * power(base, e);
-            printf("sum += 9 * %d^%d\n", base, e);
         }
         else if (s[i] == 'A')
         {
             sum += 10 * power(base, e);
-            printf("sum += 10 * %d^%d\n", base, e);
         }
         else if (s[i] == 'B')
         {
             sum += 11 * power(base, e);
-            printf("sum += 11 * %d^%d\n", base, e);
         }
         else if (s[i] == 'C')
         {
             sum += 12 * power(base, e);
-            printf("sum += 12 * %d^%d\n", base, e);
         }
         else if (s[i] == 'D')
         {
             sum += 13 * power(base, e);
-            printf("sum += 13 * %d^%d\n", base, e);
         }
         else if (s[i] == 'E')
         {
             sum += 14 * power(base, e);
-            printf("sum += 14 * %d^%d\n", base, e);
         }
         else if (s[i] == 'F')
         {
             sum += 15 * power(base, e);
-            printf("sum += 15 * %d^%d\n", base, e);
         }
         e--;
-        printf("sum: %d\n", sum);
     }
-    printf("\n %d\n", sum);
-    return sum-(base-1);
+    return sum;
 }
 
 int main() 
