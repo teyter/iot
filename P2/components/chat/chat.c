fb1760a879bd1162086cfbcd284a44dcbcd3d8d8
teyter <teitur93@gmail.com>
2025-10-01 15:18:09 +0000

    chat tell...

--- P2/components/chat/chat.c
+++ P2/components/chat/chat.c
@@ -10,6 +10,7 @@
 
 #include <lownet.h>
 #include <serial_io.h>
+#include "utility.h"
 
 #include "chat.h"
 
@@ -36,16 +37,22 @@ void tell_command(char* args)
 		serial_write_line("???\n");
 		return;
 	}
+
 	// dest = args[2] + args[3]
-	printf("dest: %s\n", args + 2);
-	/**
+	uint8_t dest = (uint8_t) hex_to_dec(strtok(args, " ") + 2);
+	char* message = strtok(NULL, "\n");
+
 	if (dest == 0)
 	{
-		serial_write_line("???\n");
+		serial_write_line("Invalid node id\n");
 		return;
 	}
-	chat_tell(args + 5, dest);
-	 */
+	if (!message)
+	{
+		serial_write_line("Invalid message\n");
+		return;
+	}
+	chat_tell(message, dest);
 }
 
 void chat_receive(const lownet_frame_t* frame) {
@@ -64,14 +71,28 @@ void chat_shout(const char* message) {
 	// ...?
 }
 
+uint8_t is_valid(const char* message)
+{
+	uint8_t i = 0;
+	int b = 1;
+	while (message[i] != '\0')
+	{
+		if (!util_printable(message[i])) return 0;
+		i++;
+	}
+	if (i > LOWNET_PAYLOAD_SIZE) return 0;
+	return i;
+}
+
 void chat_tell(const char* message, uint8_t destination) {
 
-	/**
-	printf("You: %s\n", message); // sprintf(...); serial_write_line(...)
+	uint8_t length = is_valid(message);
 
 	lownet_frame_t frame;
 	frame.source = lownet_get_device_id();
 	frame.destination = destination;
-	 */
+	frame.protocol = LOWNET_PROTOCOL_CHAT;
+	frame.length = is_valid(message);
+	// memcpy(&frame.payload, message length);
 
 }
